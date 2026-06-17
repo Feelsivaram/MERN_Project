a89661e746bc13e273b2512d5363013c2dc73aa4
Feelsivaram <46311076+Feelsivaram@users.noreply.github.com>
2026-06-17 06:57:05 +0530

    Configurations and setup - First step done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <h1>MERN React Application</h1>
-        </>
-    )
+	return (
+		<>
+			<h1>MERN React Application</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
