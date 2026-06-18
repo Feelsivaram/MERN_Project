a1f3c804d4d0d77ea09f1333205a18a4a7a56ce9
Feelsivaram <46311076+Feelsivaram@users.noreply.github.com>
2026-06-18 07:13:02 +0530

    CSS integrated successfully and created one component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<>
+			<Header />
 			<h1>MERN React Application</h1>
+			<div className="flex justify-center">
+				<div className="p-8 rounded-xl shadow-lg">
+					<span className="text-2xl font-bold text-gray-600">
+						Tailwind is Working
+					</span>
+				</div>
+			</div>
 		</>
 	);
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+function Header() {
+	return (
+		<div className="bg-gradient-to-br">
+			<Navbar />
+		</div>
+	);
+}
+
+export default Header;
